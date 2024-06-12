9b1ff9533c6449fe22eb0f579ab4f9b3427f1745
fortune-0 <fortunepeterspc07@gmail.com>
2024-06-12 05:02:03 +0100

    pycodestyle validation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -11,6 +11,6 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     result = [i async for i in async_generator()]
     return result
